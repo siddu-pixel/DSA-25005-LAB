3c8ca318a631406e3207f37fc91b7a32ea1ef1fd
siddu-pixel <sidduambadasugari@gmail.com>
2026-08-20 14:45:45 +0530

    Fix selection_sort loop range and function call
    Fixed loop range in selection_sort and corrected function call.

--- WEEK3/selectionsort.py
+++ WEEK3/selectionsort.py
@@ -1,6 +1,6 @@
 def selection_sort(arr):
     n=len(arr)
-    for i in range (n-10):
+    for i in range (n-1):
         min_index=i
         for j in range (i+1,n):
             if arr[j]<arr[min_index]:
